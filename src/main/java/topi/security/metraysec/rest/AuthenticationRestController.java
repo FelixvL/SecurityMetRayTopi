d38a8c65bbfb02033a5c6b906428048abdc1f446
FelixvL <digitaleservice@gmail.com>
2023-03-21 16:17:17 +0100

    aanmaken erbij

--- src/main/java/topi/security/metraysec/rest/AuthenticationRestController.java
+++ src/main/java/topi/security/metraysec/rest/AuthenticationRestController.java
@@ -10,12 +10,17 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import topi.security.metraysec.config.jwt.JwtAuthenticationRequest;
 import topi.security.metraysec.config.jwt.JwtAuthenticationResponse;
 import topi.security.metraysec.config.jwt.JwtTokenUtil;
 import topi.security.metraysec.config.jwt.JwtUser;
+import topi.security.metraysec.contr.UserRepository;
+import topi.security.metraysec.security.Authority;
+import topi.security.metraysec.security.AuthorityName;
+import topi.security.metraysec.security.User;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -36,6 +41,39 @@ public class AuthenticationRestController {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    @PostMapping("create")
+    public ResponseEntity<?> createUser(@RequestBody JwtAuthenticationRequest authenticationRequest) {
+        System.err.println(this.passwordEncoder.encode(authenticationRequest.getPassword()));
+
+        User user = new User();
+        user.setUsername(authenticationRequest.getUsername());
+        final String encrypedPassword = this.passwordEncoder.encode(authenticationRequest.getPassword());
+        user.setPassword(encrypedPassword);
+
+        this.userRepository.save(user);
+
+        for (AuthorityName authorityName : AuthorityName.values()) {
+            Authority authority = new Authority();
+            authority.setName(authorityName);
+            user.getAuthorities().add(authority);
+        }
+
+        this.userRepository.save(user);
+
+        System.out.printf("Created user:%s with password:%s which is bcrypted to: %s",
+                authenticationRequest.getUsername(),
+                authenticationRequest.getPassword(),
+                encrypedPassword);
+
+        return ResponseEntity.ok().build();
+    }
+    
     // try to authenticate
     @PostMapping
     public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest) throws AuthenticationException {
