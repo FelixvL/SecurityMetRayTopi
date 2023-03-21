5699ae2e320130d9c38e1a7049f656754ed80634
FelixvL <digitaleservice@gmail.com>
2023-03-21 10:54:28 +0100

    get expire date

--- src/main/java/topi/security/metraysec/rest/BootEndpoint.java
+++ src/main/java/topi/security/metraysec/rest/BootEndpoint.java
@@ -1,6 +1,7 @@
 package topi.security.metraysec.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ public class BootEndpoint {
 	@Autowired
 	BootRepository br;
 
+	@CrossOrigin
 	@GetMapping("hoi")
 	public String go() {
 		System.out.println("hoi");
